596b5ea5ce61ed346705e41b5e39f71ac23f3f58
MrAech <happy2006sharma@gmail.com>
2026-03-22 17:31:29 +0530

    The Grind Begins i guess...again

---
+++ src/main/java/com/aech/solvestuff/SolveStuffApplication.java
@@ -0,0 +1,13 @@
+package com.aech.solvestuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aech/solvestuff/SolveStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aech.solvestuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
